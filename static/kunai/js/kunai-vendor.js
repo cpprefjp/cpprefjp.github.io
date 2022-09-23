d9c77822b54663d6c9cb58b3d33e918bc6e0ab89
cpprefjp-autoupdate <shigemasa7watanabe+cpprefjp@gmail.com>
2022-09-23 13:43:25 +0000

    update automatically

--- static/kunai/js/kunai-vendor.js
+++ static/kunai/js/kunai-vendor.js
@@ -2451,7 +2451,7 @@ var CRSearch = /*#__PURE__*/function () {
                 cr_info_link = crsearch_crsearch_$('<a />');
                 cr_info_link.attr('href', CRSearch._HOMEPAGE);
                 cr_info_link.attr('target', '_blank');
-                cr_info_link.text("".concat(CRSearch._APPNAME, " v").concat({"version":"3.0.9","bugs_url":"https://github.com/cpprefjp/crsearch/issues"}.version));
+                cr_info_link.text("".concat(CRSearch._APPNAME, " v").concat({"version":"3.0.10","bugs_url":"https://github.com/cpprefjp/crsearch/issues"}.version));
                 cr_info_link.appendTo(cr_info);
                 cr_info.appendTo(result_wrapper);
                 input.on('focusin', function () {
